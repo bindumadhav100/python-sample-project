f409b49398ec02821078d21aa3c7a835d81911c9
raphael.amoedo <raphael.amoedo@infobase.com.br>
2016-09-30 17:31:50 -0300

    Adding CORS allowed and rest get by id

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 import json
+from flask_cors import CORS
 from dao import UserDao
 
 app = Flask(__name__)
+CORS(app)
 
 userDao = UserDao()
 
@@ -24,15 +26,18 @@ def create():
     userDao.save(user)
     return render_template('success.html', user=user)
 
-@app.route('/rest/', methods=['GET'])
+@app.route('/rest/', methods=['GET', 'POST'])
 def rest_get_all():
-    return jsonify(results=userDao.get_all())
-
-@app.route('/rest/create', methods=['POST'])
-def rest_create():
-    data = request.get_json()
-    user = User(**data)
-    userDao.save(user)
-    return json.dumps(user.__dict__)
+    if request.method == 'POST':
+        data = request.get_json()
+        user = User(**data)
+        userDao.save(user)
+        return json.dumps(user.__dict__)
+    else:
+        return jsonify(results=userDao.get_all())
+        
+@app.route('/rest/<int:user_id>', methods=['GET'])
+def rest_get_by_id(user_id):
+    return jsonify(results=userDao.get_by_id(user_id))
 
 app.run(port=8081)
--- dao.py
+++ dao.py
@@ -5,6 +5,15 @@ class UserDao:
 
     def save(self, user):
         self.mem_db.append(user.__dict__)
+        for i, saved_user in enumerate(self.mem_db):
+            saved_user['id'] = i
 
     def get_all(self):
         return self.mem_db[:]
+        
+    def get_by_id(self, id):
+        for user in self.mem_db:
+            print(user)
+            if user['id'] == id:
+                return user
+        return {}
